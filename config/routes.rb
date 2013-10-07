e16aec15e030eaf586c64b3c88b17af6f57480d0
Rafa Quinonero <rafa.quinonerofiguerola@xailabs.de>
2013-10-07 11:39:39 +0200

    layout i rutes acabades

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 SampleApp::Application.routes.draw do
   
+  get "users/new"
+
   root  'static_pages#home'
-  match '/help',    to: 'static_pages#help',    via: 'get'
+  match '/signup',  to: 'users#new',            via: 'get'
+  match '/help',    to: 'static_pages#help',    via: 'get' #crea rutes automaticament per a utilitzar en C i V.
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
 
